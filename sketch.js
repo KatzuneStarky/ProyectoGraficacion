c55fc4ec76016047b358eff9118442c93247f016
KatzuneStarky <79546969+KatzuneStarky@users.noreply.github.com>
2021-05-28 02:35:23 -0600

    CAMBIOS

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+let data;
+
+function preload(){
+  const url = "https://api.apify.com/v2/key-value-stores/vpfkeiYLXPIDIea2T/records/LATEST";
+  data = loadJSON(url);
+}
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(800, 500);
+  noLoop();
 }
 
 function draw() {
-
+  print(data);
 }
